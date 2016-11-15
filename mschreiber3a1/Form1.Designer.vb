bd7c28e6e7204e5c43d14070ae7abd612d3ff225
MJSchreiber <mschreiber@watlow.com>
2016-11-15 12:57:29 -0600

    started exercise mschreiber3a1

---
+++ mschreiber3a1/Form1.Designer.vb
@@ -0,0 +1,199 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnPreFor = New System.Windows.Forms.Button()
+        Me.btnPostUntil = New System.Windows.Forms.Button()
+        Me.btnPostWhile = New System.Windows.Forms.Button()
+        Me.btnPreDoUntil = New System.Windows.Forms.Button()
+        Me.btnPreDoWhile = New System.Windows.Forms.Button()
+        Me.lblEven = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btnSumForNext = New System.Windows.Forms.Button()
+        Me.btnSumUntil = New System.Windows.Forms.Button()
+        Me.btnSumWhile = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnPreFor)
+        Me.GroupBox1.Controls.Add(Me.btnPostUntil)
+        Me.GroupBox1.Controls.Add(Me.btnPostWhile)
+        Me.GroupBox1.Controls.Add(Me.btnPreDoUntil)
+        Me.GroupBox1.Controls.Add(Me.btnPreDoWhile)
+        Me.GroupBox1.Controls.Add(Me.lblEven)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(282, 214)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "p347#13 Loop 2-20"
+        '
+        'btnPreFor
+        '
+        Me.btnPreFor.Location = New System.Drawing.Point(112, 166)
+        Me.btnPreFor.Name = "btnPreFor"
+        Me.btnPreFor.Size = New System.Drawing.Size(159, 30)
+        Me.btnPreFor.TabIndex = 5
+        Me.btnPreFor.Text = "Pretest: For"
+        Me.btnPreFor.UseVisualStyleBackColor = True
+        '
+        'btnPostUntil
+        '
+        Me.btnPostUntil.Location = New System.Drawing.Point(112, 130)
+        Me.btnPostUntil.Name = "btnPostUntil"
+        Me.btnPostUntil.Size = New System.Drawing.Size(159, 30)
+        Me.btnPostUntil.TabIndex = 4
+        Me.btnPostUntil.Text = "Posttest: Loop until"
+        Me.btnPostUntil.UseVisualStyleBackColor = True
+        '
+        'btnPostWhile
+        '
+        Me.btnPostWhile.Location = New System.Drawing.Point(112, 94)
+        Me.btnPostWhile.Name = "btnPostWhile"
+        Me.btnPostWhile.Size = New System.Drawing.Size(159, 30)
+        Me.btnPostWhile.TabIndex = 3
+        Me.btnPostWhile.Text = "Posttest: Loop While"
+        Me.btnPostWhile.UseVisualStyleBackColor = True
+        '
+        'btnPreDoUntil
+        '
+        Me.btnPreDoUntil.Location = New System.Drawing.Point(112, 58)
+        Me.btnPreDoUntil.Name = "btnPreDoUntil"
+        Me.btnPreDoUntil.Size = New System.Drawing.Size(159, 30)
+        Me.btnPreDoUntil.TabIndex = 2
+        Me.btnPreDoUntil.Text = "Pretest: Do Until"
+        Me.btnPreDoUntil.UseVisualStyleBackColor = True
+        '
+        'btnPreDoWhile
+        '
+        Me.btnPreDoWhile.Location = New System.Drawing.Point(112, 22)
+        Me.btnPreDoWhile.Name = "btnPreDoWhile"
+        Me.btnPreDoWhile.Size = New System.Drawing.Size(159, 30)
+        Me.btnPreDoWhile.TabIndex = 1
+        Me.btnPreDoWhile.Text = "Pretest: Do While"
+        Me.btnPreDoWhile.UseVisualStyleBackColor = True
+        '
+        'lblEven
+        '
+        Me.lblEven.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblEven.Location = New System.Drawing.Point(11, 19)
+        Me.lblEven.Name = "lblEven"
+        Me.lblEven.Size = New System.Drawing.Size(93, 177)
+        Me.lblEven.TabIndex = 0
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.btnSumForNext)
+        Me.GroupBox2.Controls.Add(Me.btnSumUntil)
+        Me.GroupBox2.Controls.Add(Me.btnSumWhile)
+        Me.GroupBox2.Location = New System.Drawing.Point(310, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(281, 214)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "p332_348 #22 Addition"
+        '
+        'btnSumForNext
+        '
+        Me.btnSumForNext.Location = New System.Drawing.Point(115, 168)
+        Me.btnSumForNext.Name = "btnSumForNext"
+        Me.btnSumForNext.Size = New System.Drawing.Size(159, 30)
+        Me.btnSumForNext.TabIndex = 6
+        Me.btnSumForNext.Text = "Sum, For Next"
+        Me.btnSumForNext.UseVisualStyleBackColor = True
+        '
+        'btnSumUntil
+        '
+        Me.btnSumUntil.Location = New System.Drawing.Point(115, 132)
+        Me.btnSumUntil.Name = "btnSumUntil"
+        Me.btnSumUntil.Size = New System.Drawing.Size(159, 30)
+        Me.btnSumUntil.TabIndex = 5
+        Me.btnSumUntil.Text = "Sum, Loop Until"
+        Me.btnSumUntil.UseVisualStyleBackColor = True
+        '
+        'btnSumWhile
+        '
+        Me.btnSumWhile.Location = New System.Drawing.Point(115, 96)
+        Me.btnSumWhile.Name = "btnSumWhile"
+        Me.btnSumWhile.Size = New System.Drawing.Size(159, 30)
+        Me.btnSumWhile.TabIndex = 4
+        Me.btnSumWhile.Text = "Sum, Do While"
+        Me.btnSumWhile.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(112, 31)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(50, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Average:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(199, 31)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(38, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Count:"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(669, 239)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "mschreiber3a1 Loops, sum, count"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnPreFor As Button
+    Friend WithEvents btnPostUntil As Button
+    Friend WithEvents btnPostWhile As Button
+    Friend WithEvents btnPreDoUntil As Button
+    Friend WithEvents btnPreDoWhile As Button
+    Friend WithEvents lblEven As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents btnSumForNext As Button
+    Friend WithEvents btnSumUntil As Button
+    Friend WithEvents btnSumWhile As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ mschreiber3a1/Form1.vb
@@ -0,0 +1,52 @@
+﻿'Name : Loops,sum, count
+'Purpose:  creating different types of loops using pre and post test
+'Author: Mike Schreiber on 11/14/2016
+Option Strict On
+Option Explicit On
+Option Infer On
+
+Public Class frmMain
+    Private Sub btnPreDoWhile_Click(sender As Object, e As EventArgs) Handles btnPreDoWhile.Click
+        ' uses a pretest do while to count to 20
+
+        Dim i As Integer = 2
+        lblEven.Text = "Pretest Do Until" & ControlChars.NewLine
+        Do While i <= 20
+            lblEven.Text = lblEven.Text & i.ToString & ControlChars.NewLine
+            i += 2
+        Loop
+
+    End Sub
+
+    Private Sub btnPreDoUntil_Click(sender As Object, e As EventArgs) Handles btnPreDoUntil.Click
+        ' uses a pretest do until to count to 20
+
+        Dim i As Integer = 2
+        lblEven.Text = "Pretest Do Until" & ControlChars.NewLine
+        Do Until i = 22
+            lblEven.Text = lblEven.Text & i.ToString & ControlChars.NewLine
+            i += 2
+        Loop
+    End Sub
+
+    Private Sub btnPostWhile_Click(sender As Object, e As EventArgs) Handles btnPostWhile.Click
+        ' uses a post do while to count to 20 START HERE
+
+        Dim i As Integer = 2
+        lblEven.Text = "Posttest Do While" & ControlChars.NewLine
+        Do
+            lblEven.Text = lblEven.Text & i.ToString & ControlChars.NewLine
+            i += 2
+
+                End Sub
+
+    Private Sub btnPostUntil_Click(sender As Object, e As EventArgs) Handles btnPostUntil.Click
+        ' uses a post do until to count to 20
+
+
+    End Sub
+
+    Private Sub btnPreFor_Click(sender As Object, e As EventArgs) Handles btnPreFor.Click
+        ' uses a pre for next to count to 20
+    End Sub
+End Class
