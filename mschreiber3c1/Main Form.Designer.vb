5839bb77b0cdb1096974939ebf18995034c232d5
MJSchreiber <mschreiber@watlow.com>
2016-11-28 20:14:40 -0600

    finished exercise mschreiber3c1

--- mschreiber3c1/Main Form.Designer.vb
+++ mschreiber3c1/Main Form.Designer.vb
@@ -28,12 +28,24 @@ Partial Public Class frmMain
         Me.btnExit = New System.Windows.Forms.Button()
         Me.radFuncCentimeters = New System.Windows.Forms.RadioButton()
         Me.radFuncInches = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.radFunc = New System.Windows.Forms.RadioButton()
+        Me.radSub = New System.Windows.Forms.RadioButton()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.lblCalculatedWeight = New System.Windows.Forms.Label()
+        Me.cboPlanets = New System.Windows.Forms.ComboBox()
+        Me.txtWeight = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
         '
         'radSubCentimeters
         '
         Me.radSubCentimeters.Checked = True
-        Me.radSubCentimeters.Location = New System.Drawing.Point(144, 15)
+        Me.radSubCentimeters.Location = New System.Drawing.Point(129, 39)
         Me.radSubCentimeters.Margin = New System.Windows.Forms.Padding(2)
         Me.radSubCentimeters.Name = "radSubCentimeters"
         Me.radSubCentimeters.Size = New System.Drawing.Size(236, 24)
@@ -43,7 +55,7 @@ Partial Public Class frmMain
         '
         'radSubInches
         '
-        Me.radSubInches.Location = New System.Drawing.Point(144, 54)
+        Me.radSubInches.Location = New System.Drawing.Point(129, 78)
         Me.radSubInches.Margin = New System.Windows.Forms.Padding(2)
         Me.radSubInches.Name = "radSubInches"
         Me.radSubInches.Size = New System.Drawing.Size(236, 24)
@@ -53,7 +65,7 @@ Partial Public Class frmMain
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(23, 24)
+        Me.Label1.Location = New System.Drawing.Point(8, 48)
         Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(102, 20)
@@ -62,7 +74,7 @@ Partial Public Class frmMain
         '
         'txtMeasurement
         '
-        Me.txtMeasurement.Location = New System.Drawing.Point(26, 47)
+        Me.txtMeasurement.Location = New System.Drawing.Point(11, 71)
         Me.txtMeasurement.Margin = New System.Windows.Forms.Padding(2)
         Me.txtMeasurement.Name = "txtMeasurement"
         Me.txtMeasurement.Size = New System.Drawing.Size(55, 27)
@@ -71,7 +83,7 @@ Partial Public Class frmMain
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(23, 96)
+        Me.Label2.Location = New System.Drawing.Point(8, 120)
         Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(52, 20)
@@ -81,7 +93,7 @@ Partial Public Class frmMain
         'lblResult
         '
         Me.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.lblResult.Location = New System.Drawing.Point(26, 116)
+        Me.lblResult.Location = New System.Drawing.Point(11, 140)
         Me.lblResult.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.lblResult.Name = "lblResult"
         Me.lblResult.Size = New System.Drawing.Size(81, 30)
@@ -90,7 +102,7 @@ Partial Public Class frmMain
         '
         'btnExit
         '
-        Me.btnExit.Location = New System.Drawing.Point(156, 179)
+        Me.btnExit.Location = New System.Drawing.Point(173, 419)
         Me.btnExit.Margin = New System.Windows.Forms.Padding(2)
         Me.btnExit.Name = "btnExit"
         Me.btnExit.Size = New System.Drawing.Size(72, 30)
@@ -99,7 +111,7 @@ Partial Public Class frmMain
         '
         'radFuncCentimeters
         '
-        Me.radFuncCentimeters.Location = New System.Drawing.Point(144, 93)
+        Me.radFuncCentimeters.Location = New System.Drawing.Point(129, 117)
         Me.radFuncCentimeters.Margin = New System.Windows.Forms.Padding(2)
         Me.radFuncCentimeters.Name = "radFuncCentimeters"
         Me.radFuncCentimeters.Size = New System.Drawing.Size(236, 24)
@@ -108,35 +120,139 @@ Partial Public Class frmMain
         '
         'radFuncInches
         '
-        Me.radFuncInches.Location = New System.Drawing.Point(144, 132)
+        Me.radFuncInches.Location = New System.Drawing.Point(129, 156)
         Me.radFuncInches.Margin = New System.Windows.Forms.Padding(2)
         Me.radFuncInches.Name = "radFuncInches"
         Me.radFuncInches.Size = New System.Drawing.Size(236, 24)
         Me.radFuncInches.TabIndex = 9
         Me.radFuncInches.Text = "Function: Centimeters to &Inches"
         '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.radSubInches)
+        Me.GroupBox1.Controls.Add(Me.radFuncInches)
+        Me.GroupBox1.Controls.Add(Me.radSubCentimeters)
+        Me.GroupBox1.Controls.Add(Me.radFuncCentimeters)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.txtMeasurement)
+        Me.GroupBox1.Controls.Add(Me.lblResult)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Location = New System.Drawing.Point(19, 17)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(381, 207)
+        Me.GroupBox1.TabIndex = 10
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "p418 #15-16 Measurement Converter"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.radFunc)
+        Me.GroupBox2.Controls.Add(Me.radSub)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.lblCalculatedWeight)
+        Me.GroupBox2.Controls.Add(Me.cboPlanets)
+        Me.GroupBox2.Controls.Add(Me.txtWeight)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Location = New System.Drawing.Point(19, 244)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(381, 167)
+        Me.GroupBox2.TabIndex = 11
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "p426-427 #4-6 Weight on Different Planets"
+        '
+        'radFunc
+        '
+        Me.radFunc.AutoSize = True
+        Me.radFunc.Location = New System.Drawing.Point(243, 89)
+        Me.radFunc.Name = "radFunc"
+        Me.radFunc.Size = New System.Drawing.Size(83, 24)
+        Me.radFunc.TabIndex = 7
+        Me.radFunc.Text = "Function"
+        Me.radFunc.UseVisualStyleBackColor = True
+        '
+        'radSub
+        '
+        Me.radSub.AutoSize = True
+        Me.radSub.Checked = True
+        Me.radSub.Location = New System.Drawing.Point(243, 59)
+        Me.radSub.Name = "radSub"
+        Me.radSub.Size = New System.Drawing.Size(52, 24)
+        Me.radSub.TabIndex = 6
+        Me.radSub.TabStop = True
+        Me.radSub.Text = "Sub"
+        Me.radSub.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(243, 32)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(114, 20)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Calculate Using:"
+        '
+        'lblCalculatedWeight
+        '
+        Me.lblCalculatedWeight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblCalculatedWeight.Location = New System.Drawing.Point(135, 107)
+        Me.lblCalculatedWeight.Name = "lblCalculatedWeight"
+        Me.lblCalculatedWeight.Size = New System.Drawing.Size(93, 27)
+        Me.lblCalculatedWeight.TabIndex = 4
+        '
+        'cboPlanets
+        '
+        Me.cboPlanets.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboPlanets.FormattingEnabled = True
+        Me.cboPlanets.Location = New System.Drawing.Point(135, 55)
+        Me.cboPlanets.Name = "cboPlanets"
+        Me.cboPlanets.Size = New System.Drawing.Size(97, 28)
+        Me.cboPlanets.TabIndex = 3
+        '
+        'txtWeight
+        '
+        Me.txtWeight.Location = New System.Drawing.Point(23, 55)
+        Me.txtWeight.Name = "txtWeight"
+        Me.txtWeight.Size = New System.Drawing.Size(93, 27)
+        Me.txtWeight.TabIndex = 2
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(135, 32)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(81, 20)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "Weight on:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(23, 32)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(95, 20)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Earth weight:"
+        '
         'frmMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(402, 225)
-        Me.Controls.Add(Me.radFuncInches)
-        Me.Controls.Add(Me.radFuncCentimeters)
+        Me.ClientSize = New System.Drawing.Size(418, 460)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.btnExit)
-        Me.Controls.Add(Me.lblResult)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.txtMeasurement)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.radSubInches)
-        Me.Controls.Add(Me.radSubCentimeters)
         Me.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.MaximizeBox = False
         Me.Name = "frmMain"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "mschreiber3c1: Measurement Converter"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
     Friend WithEvents radSubCentimeters As System.Windows.Forms.RadioButton
@@ -148,4 +264,14 @@ Partial Public Class frmMain
     Friend WithEvents btnExit As System.Windows.Forms.Button
     Friend WithEvents radFuncCentimeters As RadioButton
     Friend WithEvents radFuncInches As RadioButton
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents radFunc As RadioButton
+    Friend WithEvents radSub As RadioButton
+    Friend WithEvents Label6 As Label
+    Friend WithEvents lblCalculatedWeight As Label
+    Friend WithEvents cboPlanets As ComboBox
+    Friend WithEvents txtWeight As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
 End Class
--- mschreiber3c1/Main Form.vb
+++ mschreiber3c1/Main Form.vb
@@ -35,6 +35,63 @@ Public Class frmMain
         Return dblResult
     End Function
 
+    'calculates earth weight on selected planet using a sub procedure
+    Private Sub CalcWeightSub(ByVal intWeight As Integer,
+                              ByRef dblWeight As Double)
+        'do some calculations here
+        Dim dblGravity As Double
+
+        Select Case cboPlanets.Text
+            Case "Mercury", "Mars"
+                dblGravity = 0.38
+            Case "Venus"
+                dblGravity = 0.91
+            Case "Earth"
+                dblGravity = 1
+            Case "Jupiter"
+                dblGravity = 2.34
+            Case "Saturn"
+                dblGravity = 1.06
+            Case "Uranus"
+                dblGravity = 0.92
+            Case "Neptune"
+                dblGravity = 1.14
+            Case "Pluto"
+                dblGravity = 0.06
+        End Select
+
+        dblWeight = intWeight * dblGravity
+    End Sub
+
+    'calculates earth weight on selected planet using a function
+    Private Function CalcWeightFunc(ByVal intEarthWeight As Integer) As Double
+
+
+        Dim dblGravity As Double
+
+        Select Case cboPlanets.Text
+            Case "Mercury", "Mars"
+                dblGravity = 0.38
+            Case "Venus"
+                dblGravity = 0.91
+            Case "Earth"
+                dblGravity = 1
+            Case "Jupiter"
+                dblGravity = 2.34
+            Case "Saturn"
+                dblGravity = 1.06
+            Case "Uranus"
+                dblGravity = 0.92
+            Case "Neptune"
+                dblGravity = 1.14
+            Case "Pluto"
+                dblGravity = 0.06
+        End Select
+
+        Dim dblWeight As Double = intEarthWeight * dblGravity
+
+        Return dblWeight
+    End Function
 
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
         Me.Close()
@@ -59,7 +116,7 @@ Public Class frmMain
         Dim dblMeasurement As Double = 0.0
         Dim dblResult As Double = 0.0
 
-        'txtMeasurement.Text = dblMeasurement.ToString("n2")
+
         Double.TryParse(txtMeasurement.Text, dblMeasurement)
 
 
@@ -86,6 +143,56 @@ Public Class frmMain
     Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'uses 10.00 for the initial value for txtMeasreument.text
         txtMeasurement.Text = "10.00"
+
+        'loads planets to cboPlanets
+        cboPlanets.Items.Add("Mercury")
+        cboPlanets.Items.Add("Mars")
+        cboPlanets.Items.Add("Venus")
+        cboPlanets.Items.Add("Earth")
+        cboPlanets.Items.Add("Jupiter")
+        cboPlanets.Items.Add("Saturn")
+        cboPlanets.Items.Add("Uranus")
+        cboPlanets.Items.Add("Neptune")
+        cboPlanets.Items.Add("Pluto")
+        cboPlanets.SelectedItem = "Earth"
+        txtWeight.Text = "100"
+    End Sub
+
+    Private Sub txtWeight_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtWeight.KeyPress
+        ' allow only numbers, the period, and the Backspace key
+
+        If (e.KeyChar < "0" OrElse e.KeyChar > "9") AndAlso e.KeyChar <> "." AndAlso e.KeyChar <> ControlChars.Back Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub txtWeight_Enter(sender As Object, e As EventArgs) Handles txtWeight.Enter
+        'selects all the text 
+
+        txtWeight.SelectAll()
+    End Sub
+
+    Private Sub InputsChanged(sender As Object, e As EventArgs) _
+        Handles txtWeight.TextChanged, cboPlanets.SelectedIndexChanged,
+        radFunc.CheckedChanged, radSub.CheckedChanged
+
+        Dim intEarthWeight As Integer
+        Dim dblWeight As Double
+
+        Integer.TryParse(txtWeight.Text, intEarthWeight)
+
+        If radSub.Checked Then
+            CalcWeightSub(intEarthWeight, dblWeight)
+
+        ElseIf radFunc.Checked Then
+            dblWeight = CalcWeightFunc(intEarthWeight)
+        End If
+
+        'displays the converted result
+        lblCalculatedWeight.Text = dblWeight.ToString("n2")
     End Sub
 End Class
+
+
+
 
