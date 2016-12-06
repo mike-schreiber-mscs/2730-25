e8e6fe71aa40cf53d85cfd94531f39ae5bf865bd
MJSchreiber <mschreiber@watlow.com>
2016-12-05 21:11:03 -0600

    added more to mschreiber3e1.  Need to ask about function in part 1 of assignment

--- mschreiber3e1/Form1.Designer.vb
+++ mschreiber3e1/Form1.Designer.vb
@@ -22,135 +22,138 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
-        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.cboSearchValue = New System.Windows.Forms.ComboBox()
+        Me.cboSearchRange = New System.Windows.Forms.ComboBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblCommissionValues = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.Label6 = New System.Windows.Forms.Label()
-        Me.Label7 = New System.Windows.Forms.Label()
-        Me.Label8 = New System.Windows.Forms.Label()
+        Me.lblSearchCount = New System.Windows.Forms.Label()
+        Me.lblSearchRangeCount = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'ComboBox1
+        'cboSearchValue
         '
-        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(96, 102)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(121, 150)
-        Me.ComboBox1.TabIndex = 0
+        Me.cboSearchValue.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple
+        Me.cboSearchValue.FormattingEnabled = True
+        Me.cboSearchValue.Items.AddRange(New Object() {"50", "100", "300", "900"})
+        Me.cboSearchValue.Location = New System.Drawing.Point(122, 104)
+        Me.cboSearchValue.Name = "cboSearchValue"
+        Me.cboSearchValue.Size = New System.Drawing.Size(121, 85)
+        Me.cboSearchValue.TabIndex = 0
         '
-        'ComboBox2
+        'cboSearchRange
         '
-        Me.ComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple
-        Me.ComboBox2.FormattingEnabled = True
-        Me.ComboBox2.Location = New System.Drawing.Point(96, 258)
-        Me.ComboBox2.Name = "ComboBox2"
-        Me.ComboBox2.Size = New System.Drawing.Size(121, 150)
-        Me.ComboBox2.TabIndex = 1
+        Me.cboSearchRange.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple
+        Me.cboSearchRange.FormattingEnabled = True
+        Me.cboSearchRange.Items.AddRange(New Object() {"   0 - 200", "100 - 300", "700  - 800"})
+        Me.cboSearchRange.Location = New System.Drawing.Point(122, 201)
+        Me.cboSearchRange.Name = "cboSearchRange"
+        Me.cboSearchRange.Size = New System.Drawing.Size(121, 72)
+        Me.cboSearchRange.TabIndex = 1
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(12, 10)
+        Me.Label1.Location = New System.Drawing.Point(18, 12)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.Size = New System.Drawing.Size(99, 13)
         Me.Label1.TabIndex = 2
-        Me.Label1.Text = "Label1"
+        Me.Label1.Text = "Commission values:"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(12, 105)
+        Me.Label2.Location = New System.Drawing.Point(18, 107)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.Size = New System.Drawing.Size(88, 13)
         Me.Label2.TabIndex = 3
-        Me.Label2.Text = "Label2"
+        Me.Label2.Text = "Search for value:"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(12, 262)
+        Me.Label3.Location = New System.Drawing.Point(18, 204)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.Size = New System.Drawing.Size(89, 13)
         Me.Label3.TabIndex = 4
-        Me.Label3.Text = "Label3"
+        Me.Label3.Text = "Search for range:"
         '
-        'Label4
+        'lblCommissionValues
         '
-        Me.Label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.Label4.Location = New System.Drawing.Point(96, 9)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(287, 74)
-        Me.Label4.TabIndex = 5
+        Me.lblCommissionValues.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblCommissionValues.Location = New System.Drawing.Point(122, 11)
+        Me.lblCommissionValues.Name = "lblCommissionValues"
+        Me.lblCommissionValues.Size = New System.Drawing.Size(266, 74)
+        Me.lblCommissionValues.TabIndex = 5
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(236, 105)
+        Me.Label5.Location = New System.Drawing.Point(255, 107)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(39, 13)
+        Me.Label5.Size = New System.Drawing.Size(38, 13)
         Me.Label5.TabIndex = 6
-        Me.Label5.Text = "Label5"
+        Me.Label5.Text = "Count:"
         '
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(236, 261)
+        Me.Label6.Location = New System.Drawing.Point(255, 205)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(39, 13)
+        Me.Label6.Size = New System.Drawing.Size(38, 13)
         Me.Label6.TabIndex = 7
-        Me.Label6.Text = "Label6"
+        Me.Label6.Text = "Count:"
         '
-        'Label7
+        'lblSearchCount
         '
-        Me.Label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.Label7.Location = New System.Drawing.Point(281, 104)
-        Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(102, 30)
-        Me.Label7.TabIndex = 8
+        Me.lblSearchCount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblSearchCount.Location = New System.Drawing.Point(299, 106)
+        Me.lblSearchCount.Name = "lblSearchCount"
+        Me.lblSearchCount.Size = New System.Drawing.Size(89, 30)
+        Me.lblSearchCount.TabIndex = 8
         '
-        'Label8
+        'lblSearchRangeCount
         '
-        Me.Label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.Label8.Location = New System.Drawing.Point(281, 261)
-        Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(102, 30)
-        Me.Label8.TabIndex = 9
+        Me.lblSearchRangeCount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblSearchRangeCount.Location = New System.Drawing.Point(299, 204)
+        Me.lblSearchRangeCount.Name = "lblSearchRangeCount"
+        Me.lblSearchRangeCount.Size = New System.Drawing.Size(89, 30)
+        Me.lblSearchRangeCount.TabIndex = 9
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(407, 420)
-        Me.Controls.Add(Me.Label8)
-        Me.Controls.Add(Me.Label7)
+        Me.ClientSize = New System.Drawing.Size(407, 278)
+        Me.Controls.Add(Me.lblSearchRangeCount)
+        Me.Controls.Add(Me.lblSearchCount)
         Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.lblCommissionValues)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.ComboBox2)
-        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.cboSearchRange)
+        Me.Controls.Add(Me.cboSearchValue)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "mschreiber3e1 Arrays, Functions"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents ComboBox1 As ComboBox
-    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents cboSearchValue As ComboBox
+    Friend WithEvents cboSearchRange As ComboBox
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
-    Friend WithEvents Label4 As Label
+    Friend WithEvents lblCommissionValues As Label
     Friend WithEvents Label5 As Label
     Friend WithEvents Label6 As Label
-    Friend WithEvents Label7 As Label
-    Friend WithEvents Label8 As Label
+    Friend WithEvents lblSearchCount As Label
+    Friend WithEvents lblSearchRangeCount As Label
 End Class
--- mschreiber3e1/Form1.vb
+++ mschreiber3e1/Form1.vb
@@ -1,3 +1,78 @@
-﻿Public Class Form1
+﻿'Name: mschreiber3e1
+'Purpose: Displays counts of salespeople commissions
+'Author: Mike Schreiber on Dec 5 2016
+
+Option Strict On
+Option Explicit On
+Option Infer On
+
+
+Public Class Form1
+
+    'array to populate commission values
+    Private intCommission() As Integer = {300, 500, 200, 150, 600, 750,
+           90, 150, 100, 200, 250, 650, 300, 750, 800, 350, 250, 150, 100, 300}
+
+    'sub procedure that iterates thru the intcommission array and populates the lbl with values
+
+    Private Sub DisplayCommissions()
+        Dim intCount1 As Integer
+        Dim intHighSub As Integer = intCommission.GetUpperBound(0)
+
+        Do While intCount1 <= intHighSub
+            lblCommissionValues.Text &= intCommission(intCount1).ToString & ", "
+            intCount1 += 1
+        Loop
+
+        If lblCommissionValues.Text.EndsWith(", ") Then
+            lblCommissionValues.Text = lblCommissionValues.Text.Substring(0, lblCommissionValues.Text.Length - 2)
+        End If
+    End Sub
+
+    'form load calls DisplayCommissions sub procedure
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DisplayCommissions()
+    End Sub
+
+    'calculates the count value by interating thru the intcommissions value array and compares search value to 
+    'each array element  Then increments the counter by 1 each time a match happens
+
+    Private Function CountValue(ByVal intSearchValue As Integer) As Integer
+        Dim intCount1 As Integer
+        Dim intCount2 As Integer
+        Dim intMatchCount As Integer
+
+        Do While intCount2 <= intCommission.GetUpperBound(0)
+            'THIS IS NOT CORRECT... NEED SOMETHING IN HERE TO ASSIGN EACH ARRAY ELEMENT BEFORE COMPARING
+            If intSearchValue = intCommission(intCount1) Then
+                intMatchCount += 1
+            Else
+                intMatchCount = 1
+            End If
+            intCount2 += 1
+        Loop
+
+        Return intMatchCount
+    End Function
+
+    'text changed event Does not work. Returns 1.  NEED TO FIX FUNCTION AND RERUN
+    Private Sub cboSearchValue_TextChanged(sender As Object, e As EventArgs) Handles cboSearchValue.TextChanged
+        Dim strInput As String = cboSearchValue.Text
+        Dim intSearchValue As Integer
+        Dim intCount As Integer
+
+        'converts user input or comboBox selection into an integer
+        Integer.TryParse(strInput, intSearchValue)
+
+        'Checks for valid user input and if so... calls the CountValue Function otherwise it shows a message to user
+        If intSearchValue >= 0 AndAlso intSearchValue <= 1000 Then
+            lblSearchCount.Text = CountValue(intCount).ToString
+
+        Else
+            MessageBox.Show("Please enter an integer between 0 and 1000", "Invalid Entry",
+                            MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        End If
+    End Sub
 
 End Class
+
